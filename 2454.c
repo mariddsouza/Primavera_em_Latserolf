02d24e5b0f738632420c2e5d37de113d0a95f214
Mariana <marianadesouza.cnt@gmail.com>
2020-10-16 13:29:10 -0300

    update

---
+++ 2454.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a==0){
+        printf("C\n");
+    }
+    else{
+        if(b==0){
+            printf("B\n");
+        }
+        else
+        {
+            printf("A\n");
+        }
+        
+    }
+
+    return 0;
+}
